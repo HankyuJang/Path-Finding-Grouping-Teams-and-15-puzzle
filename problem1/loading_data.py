8774665d6b33d673d68fbc304faee91be0613c08
Pulkit Maloo <maloop@iu.edu>
2017-09-21 13:02:30 -0400

    Handled Ferry cases

--- problem1/loading_data.py
+++ problem1/loading_data.py
@@ -4,8 +4,12 @@ Created on Wed Sep 20 17:31:29 2017
 
 @author: PulkitMaloo
 """
-# Assumption speed = 45 for missing or 0 values since majority of the
-# road segments have speed 45
+# Assumption 1
+# speed = 45 for missing or 0 values
+# since majority of the road segments seem to have speed 45
+
+# Assumption 2
+# When speed=0 and distance=0 there is no route possible
 
 from __future__ import division
 import sys
@@ -13,7 +17,8 @@ import numpy as np
 
 
 # ==============================================================================
-# The format of data is:
+#   The format of data is:
+#
 # data =
 #   {
 #   city:
@@ -57,6 +62,10 @@ def reading_files():
         if len(seg) != 5:
              seg = seg[:3] + [None] + seg[3:]
 
+        # Ferry cases where speed=0 and distance=0
+        if int(seg[2])==0 and int(seg[3])==0:
+            continue
+
         # from_city to to_city
         if 'to_city' not in data[seg[0]]:
             data[seg[0]]['to_city'] = dict()
@@ -73,20 +82,28 @@ def reading_files():
             data[seg[1]]['to_city'] = dict()
         data[seg[1]]['to_city'][seg[0]] = {'dist': seg[2], 'speed': seg[3], 'highway': seg[4]}
 
+        #Delete this
+        if int(data[seg[1]]['to_city'][seg[0]]['dist'])==0:
+            import pandas as pd
+            print(data[seg[1]]['latitude'], data[seg[1]]['longitude'])
+
     f_city.close()
     f_road.close()
     return data
 
 def lat_lon_distance(from_city, to_city):
-#### This function is copied from ######################################
+# This function is copied from the following website
 # https://www.w3resource.com/python-exercises/math/python-math-exercise-27.php
     from math import radians, sin, cos, acos
-    slat = radians(float(data[from_city]['latitude']))
-    slon = radians(float(data[from_city]['longitude']))
-    elat = radians(float(data[to_city]['latitude']))
-    elon = radians(float(data[to_city]['longitude']))
-    dist = 6371.01 * acos(sin(slat)*sin(elat) + cos(slat)*cos(elat)*cos(slon - elon))
-    return round(dist, 2)
+    try:
+        slat = radians(float(data[from_city]['latitude']))
+        slon = radians(float(data[from_city]['longitude']))
+        elat = radians(float(data[to_city]['latitude']))
+        elon = radians(float(data[to_city]['longitude']))
+        dist = 6371.01 * acos(sin(slat)*sin(elat) + cos(slat)*cos(elat)*cos(slon - elon))
+        return round(dist, 2)
+    except:
+        return
 
 def distance(from_city, to_city):
     # Handle distance = 0
@@ -119,18 +136,28 @@ def successors(city):
     return data[city]['to_city'].keys()
 
 def solve(start_city):
+    # Paths is is a list of path where each path can explored further
     paths = [[start_city]]
+    # List of the visited cities
     visited = []
+    # While there are still paths to be explored
     while paths:
+        # Retrieve a path according to BFS or DFS
         # use pop() for DFS
         path = paths.pop(0)
+        # Retreive the last city from the path to be expanded
         curr = path[-1]
+        # For all cities that we can go from the current city
         for next_city in successors(curr):
+            # And if the city is not already visited
             if next_city in visited:
                 continue
+            # Check if it's our goal state then return the path
             if is_goal(next_city):
                 return path + [next_city]
+            # Add this current city to our list of visited cities
             visited.append(next_city)
+            # Add the new expanded path to our paths list
             new_path = path + [next_city]
             paths.append(new_path)
 
