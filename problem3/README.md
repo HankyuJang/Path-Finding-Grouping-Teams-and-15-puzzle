15f7f4eee009705d893883249ea4f704fa303aed
Hankyu Jang <jhkmath@gmail.com>
2017-09-26 17:21:12 -0400

    Modify solver16.py - Now working in BFS.

---
+++ problem3/README.md
@@ -0,0 +1,15 @@
+## Method
+
+Currently working model using BFS.
+(I just made it working... the code is dirty now)
+
+## Usage
+
+Following lines tests on the three different input boards.
+
+```
+./solver16.py input_board.txt
+./solver16.py input_board.txt
+./solver16.py input_board.txt
+```
+
--- problem3/solver16.py
+++ problem3/solver16.py
@@ -4,19 +4,12 @@
 import sys
 import numpy as np
 
-# pos_move = {0: (L11, L21, L31, U11, U21, U31),
-            # 1: (R12, L12, L22, U11, U21, U31),
-            # 2: (R12, L12, L22, U11, U21, U31),
-            # 3: (R12, L12, L22, U11, U21, U31),
-            # }
-
 col_move = {0:["L11","L21","L31"],1:["R12","L12","L22"],2:["R13","R23","L13"],3:["R14","R24","R34"]}
 row_move = {0:["U11","U21","U31"],1:["D12","U12","U22"],2:["D13","D23","U13"],3:["D14","D24","D34"]}
 
 def initial_state(filename):
     file = open(filename, "r")
     return np.array([[int(tile) for tile in line.split()] for line in file])
-
     
 def swap(s, r1, c1, r2, c2):
     temp = s[r1,c1]
@@ -52,13 +45,37 @@ def successor(s):
             for i in range(n):
                 swap(s_next,row,col,row,col+1)
                 col += 1
-        successor_list.append(s_next)
+        successor_list.append((s_next, move))
 
     return successor_list
     
+def is_goal(s):
+    return (s == np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]])).all()
+
+def solve(initial_board):
+    global puzzle_tracking
+    fringe = [initial_board]
+    while len(fringe) > 0:
+        s = fringe.pop(0)
+        for s_prime, move in successor(s):
+            if to_str(s_prime) in puzzle_tracking:
+                if len(move) < len(puzzle_tracking[to_str(s_prime)]):
+                    puzzle_tracking[to_str(s_prime)] = move
+                continue
+            else:
+                puzzle_tracking[to_str(s_prime)] = puzzle_tracking[to_str(s)] + [move]
+            if is_goal(s_prime):
+                return(puzzle_tracking[to_str(s_prime)])
+            fringe.append(s_prime)
+    return False
+
+# This is to use each state as the key of the dictionary to keep track of the moves
+def to_str(s):
+    return ''.join(str(elem) for inner in s for elem in inner)
 
 filename = sys.argv[1]
 S0 = initial_state(filename)
-Sprime = successor(S0)
-print Sprime
+puzzle_tracking = {to_str(S0): []}
+
+print solve(S0)
 
